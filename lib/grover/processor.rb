b96d9e09287547b62c96f38ffebe47bcc0b5d146
Matheus Sales <matheus_usales@hotmail.com>
2025-01-07 17:15:58 -0300

    fix: Try to fix for Ruby 3.4 compatibility

--- lib/grover/processor.rb
+++ lib/grover/processor.rb
@@ -21,7 +21,7 @@ class Grover
       return unless result
       return result if result.is_a?(String)
 
-      result['data'].pack('C*')
+      result.values.map(&:to_i).pack('C*')
     ensure
       cleanup_process if stdin
     end
